0c1088b4fb63aa78b1d0e7d2b8cbcefb217e0ceb
brandan.neises <brandan.neises@phantom.us>
2018-10-18 12:22:56 -0500

    PAPP-5130 Ran correct update_cr.py.

--- parser_connector.py
+++ parser_connector.py
@@ -1,7 +1,7 @@
 # File: parser_connector.py
 # Copyright (c) 2017-2018 Splunk Inc.
 #
-# SPLUNK CONFIDENTIAL – Use or disclosure of this material in whole or in part
+# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
 # without a valid written license from Splunk Inc. is PROHIBITED.
 # Phantom App imports
 import phantom.app as phantom
